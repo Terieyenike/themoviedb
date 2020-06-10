aa6fd1169127eb12e0648a13736ce79377c9969a
Oteri Eyenike <teyenike1@gmail.com>
2020-06-10 03:00:47 +0100

    Updates

---
+++ precache-manifest.55a4d8e49a874eb20b0ec0d1812871d1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d409aebb3d6916ecb3fc508b953863f5",
+    "url": "/themoviedb/index.html"
+  },
+  {
+    "revision": "3cae8f55fcba01e7ef88",
+    "url": "/themoviedb/static/css/main.20e0feca.chunk.css"
+  },
+  {
+    "revision": "f83d9c11248bb42d0a4b",
+    "url": "/themoviedb/static/js/2.24afc979.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/themoviedb/static/js/2.24afc979.chunk.js.LICENSE"
+  },
+  {
+    "revision": "3cae8f55fcba01e7ef88",
+    "url": "/themoviedb/static/js/main.6cbfabef.chunk.js"
+  },
+  {
+    "revision": "b81a335c2b2df3517d9b",
+    "url": "/themoviedb/static/js/runtime-main.274396bf.js"
+  }
+]);
