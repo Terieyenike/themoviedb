cb01ead6e113cf8aa5074d65778ac1eb67c57c01
Oteri Eyenike <teyenike1@gmail.com>
2020-06-10 02:47:25 +0100

    uploaded the favicon and the search movie db app for movies

---
+++ src/components/searchMovies.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { MovieCard } from './movieCard';
+
+const SearchMovies = () => {
+  const [query, setQuery] = useState('');
+  const [movies, setMovies] = useState([]);
+
+  const searchMovies = async e => {
+    e.preventDefault();
+    const url = `https://api.themoviedb.org/3/search/movie?api_key=e03da57cdb6ac698fe2f86dd70dd02d6&language=en-US&query=${query}&page=1&include_adult=false`;
+    try {
+      const res = await fetch(url);
+      const data = await res.json();
+      setMovies(data.results);
+    } catch (err) {
+      console.log(err, err.message);
+    }
+  };
+
+  const onSearchChange = e => {
+    setQuery(e.target.value);
+  };
+
+  return (
+    <>
+      <form className='form' onSubmit={searchMovies}>
+        <label htmlFor='query' className='label'>
+          Movie Name
+        </label>
+        <input
+          aria-label="Search for blockbuster movies"
+          type='text'
+          className='input'
+          name='query'
+          placeholder='i.e. Jurassic Park'
+          value={query}
+          onChange={onSearchChange}
+        />
+        <button className='button' type='submit'>
+          Search
+        </button>
+      </form>
+      <div className='card-list'>
+        {movies
+          .filter(movie => movie.poster_path)
+          .map(movie => (
+            <MovieCard movie={movie} key={movie.id} />
+          ))}
+      </div>
+    </>
+  );
+};
+
+export default SearchMovies;
